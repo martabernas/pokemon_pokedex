b4ab6b44225270ddca6dbabb3f14503d1eeb185f
Marta Bernas <marta.bernas1996@gmail.com>
2023-04-15 22:16:23 +0200

    Add table to web application

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup
 import re
 import requests
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -14,13 +14,13 @@ def main():
     soup = BeautifulSoup(req.content, "html.parser")
     names = []
     numbers = []
-    types_eng = []
+    types_pl = []
     html_string = ""
     types_dict = {"normal": "normalny", "fighting": "walczący", "flying": "latający",
-                  "poison": "trujący", "ground": "ziemny", "rock": "kamienny", "bug": "robaczy",
-                  "ghost": "duchowy", "steel": "stalowy", "fire": "ognisty", "water": "wodny",
+                  "poison": "trujący", "ground": "ziemny", "rock": "kamienny", "bug": "robak",
+                  "ghost": "duch", "steel": "stalowy", "fire": "ognisty", "water": "wodny",
                   "grass": "trawiasty", "electric": "elektryczny", "psychic": "psychiczny",
-                  "ice": "lodowy", "dragon": "smoczy", "dark": "mroczny"}
+                  "ice": "lodowy", "dragon": "smok", "dark": "mroczny"}
 
     for tag in soup.find_all(class_="ent-name"):
         names.append(tag.string)
@@ -31,15 +31,7 @@ def main():
     for name in names:
         pokemon_types = soup.find(string=name).find_parent().find_next_sibling("small").find_all(
             class_="itype")
-        types_eng.append(
+        types_pl.append(
             [types_dict[pokemon_type.string.casefold()] for pokemon_type in pokemon_types])
 
-    for number, name, type_ in zip(numbers, names, types_eng):
-        html_string += f"{number} {name} {type_} <br>"
-        print(number, name, type_)
-
-    return html_string
-
-
-if __name__ == '__main__':
-    main()
+    return render_template("index.html", names=names, numbers=numbers, types=types_pl, zip=zip)
